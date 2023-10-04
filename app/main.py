fef7654a4028d6a3c2a2c58eedf2d8e8ea6e1def
chanelp <mariannissanchez27@gmail.com>
2023-10-04 17:38:36 -0400

    Módulos como scripts

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 import utils
 
-print(utils.get_population())
-
 data = [{
-	'Country': 'Colombia',
-	'Population': 500
-},
-{
-	'Country': 'Bolivia',
-	'Population': 300
-}
-]
+		'Country': 'Colombia',
+		'Population': 500
+	},
+	{
+		'Country': 'Bolivia',
+		'Population': 300
+	}
+	]
 
-country = input("Type country: ")
-result = utils.population_by_country(data, country)
-print(result)
+def run():
+	print(utils.get_population())
+
+
+	country = input("Type country: ")
+	country =  country.capitalize()
+
+	result = utils.population_by_country(data, country)
+	print(result)
+
+# Este if nos da la posibilidad de manejar el archivo como un script, desde la terminal.
+if __name__ == "__main__":
+	run()
