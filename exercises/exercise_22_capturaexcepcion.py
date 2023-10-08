2de4e527c9d57907717cbea16c223a49af1419e5
chanelp <mariannissanchez27@gmail.com>
2023-10-07 21:18:19 -0400

    Ejercicio Manejo de excepciones

--- exercises/exercise_22_capturaexcepcion.py
+++ exercises/exercise_22_capturaexcepcion.py
@@ -1,10 +1,9 @@
 def my_divide(a, b):
-   # Escribe tu solución 👇
    try:
       result = a / b
-      return result
    except ZeroDivisionError as error:
-      return 'No se puede dividir por 0'
+      result = 'No se puede dividir por 0'
+   return result
     
 response = my_divide(10, 2)
 print(response) # 5
