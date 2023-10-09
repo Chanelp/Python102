1037535774d85ebe8194c1aeb05df075ed8e7e02
chanelp <mariannissanchez27@gmail.com>
2023-10-09 10:42:28 -0400

    Leer un csv para calcular el total de gastos simplificado

--- app/total_gastos_csv.py
+++ app/total_gastos_csv.py
@@ -2,13 +2,8 @@ import csv
 
 def read_csv(path):
    with open(path, 'r') as csvfile:
-      reader = csv.reader(csvfile, delimiter=',')
-      total = 0
-
-      for row in reader:
-        total += int(row[1])
-        
-   return total
+       total = sum(int(row[1]) for row in csv.reader(csvfile))
+       return total
 
 if __name__ == "__main__":
     response = read_csv('./app/gastos.csv')
