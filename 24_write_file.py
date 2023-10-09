a9e760d6dce96d1dce8dc067aac1c32c11488d8b
chanelp <mariannissanchez27@gmail.com>
2023-10-09 09:29:53 -0400

    Sobre escribir en el archivo

--- 24_write_file.py
+++ 24_write_file.py
@@ -1,11 +1,11 @@
-# Escribir en un archivo
+# Escribir en un archivo con r+
 with open("./text.txt", "r+") as file:
     for line in file:
         print(line)
     file.write("\nIngles basico")
     file.write("\nLibro para aprender ignles >D")
 
-# Sobre-escribir en el archivo
+# Sobre-escribir un archivo con w+
 with open("./text.txt", 'w+') as file:
     for line in file:
         print(line)
